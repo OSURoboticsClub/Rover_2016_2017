8643bb7efa6c3b60246e84a73dfce02b7893cfe4
Nick Ames <nick@fetchmodus.org>
2016-08-23 21:28:06 -0700

    Got send command code generation working in docparse (not yet tested, but it looks fine).

--- etc/docparse.py
+++ etc/docparse.py
@@ -64,19 +64,63 @@ def format_code_to_cstdint(fmt_name):
 	else:
 		raise ValueError("Unknown format code \"%s\""%fmt_name)
 
+def format_code_size(fmt_name):
+	"""Return the number of bytes required for a type with the given format code."""
+	if fmt_name == "*":
+		return 255
+	else:
+		return int(fmt_name[1:])/8
+	
 def send_prototype(cmd_dict):
 	"""Given a command dictionary (from extract_table), return a function that constructs
 	   a packet of that type. The function relies on the following function:
 	     send(uint8_t *data, uint16_t count) - send the given packet across the radio link.
 	                                           The send() function must add a start byte and 
-	                                           escape start and escape characters where necessary."""
-	print cmd_dict
+	                                           escape start and escape characters where necessary.
+	     pack*(void *data, uint16_t pos) - pack the given value (where * = 8, 16, 32, or 64 bits)
+	                                         into the given buffer position in a little-endian format.
+	     memcpy(void *dest, void *src, uint16_t count) - copy bytes"""
+	cmd = cmd_dict
+	#Prototype
+	proto = "void "
+	proto += "send_" + cmd["name"].lower().replace(" ", "_") + "("
+	for a in cmd["argument"]:
+		proto += format_code_to_cstdint(a[0]) + " " + a[1] + ", "
+	proto = proto[0:-2] #Remove last commma 
+	proto += "){\n"
 	
+	#Packet stuffing
+	body = ""
+	totalsize = 0 #current byte in packet buffer
+	prev_a = None
+	for a in cmd["argument"]:
+		if a[0] == "*":
+			if prev_a == None:
+				raise ValueError("In command %s, variable-length argument used before length controlling argument.")
+			body += "\tmemcpy(buf + b, %s, %s);\n"%(a[1], prev_a[1])
+			body += "\tb += %s;\n"%prev_a[1]
+			totalsize += 255
+		else:
+			s = format_code_size(a[0])
+			body += "\tpack%d(&%s, buf + b);\n"%(s*8, a[1])
+			body += "\tb += %d;\n"%s
+			totalsize += s
+		prev_a = a
+		
+	#Send command
+	send = "\tsend(buf, b);\n}\n"
+		
+	#Declarations
+	declarations = "\tuint16_t b = 0;\n\tuint8_t buf[%d];\n"%totalsize
+	
+	return proto + declarations + body + send
+
 if __name__ == "__main__":
 	import sys
 	with open(sys.argv[1], "r") as f:
 		cmds = extract_table(f.read())
-		send_prototype(cmds[3])
+		for c in cmds:
+			print send_prototype(c)
 		
 
 

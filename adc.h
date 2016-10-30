32f04de961f4c16a257b958d801df10174fd0d51
aaronschraner <aaron@pnwcab.com>
2016-10-30 08:39:57 -0700

    Added ADC module (#1)
    Merged initial ADC module implementation.

--- adc.h
+++ adc.h
@@ -8,7 +8,7 @@
 #include <stdint.h>
 
 /* ADC Reference Options */
-#define ADC_REF_RATIOMETRIC 0 /* ADC Reference is 5V AVcc voltage. */
+#define ADC_REF_RATIOMETRIC 1 /* ADC Reference is 5V AVcc voltage. */
 #define ADC_REF_2V56 3 /* ADC Reference is 2.56V internal reference. */
 #define ADC_REF_1V1 2 /* ADC Reference is 1.1V internal reference. */
 #define ADC_CLK_PRESCALE 5 /* ADC clock prescaler (change if you need faster response) */

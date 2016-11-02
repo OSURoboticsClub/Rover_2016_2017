5b95e05f7ca153b28a162b5b96bc4d931c850995
nikitaroz <rozanovn@oregonstate.edu>
2016-11-01 18:18:18 -0700

    Added base_packets.py, comments to code

---
+++ common/base_packets.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''This packet generates C files for handling packets on the 
+side of the base station.'''
+
+import codegen
+
+def main():
+    '''This function will generate some files that
+    the base station can use for handling packets'''
+    pass
+    
+if __name__ == "__main__":
+    main()

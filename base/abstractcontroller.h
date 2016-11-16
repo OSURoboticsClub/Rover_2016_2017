03c3bf14b24a54acbd0db80c44911641aa9e9986
nikitaroz <rozanovn@oregonstate.edu>
2016-11-15 19:21:59 -0800

    added comment

--- base/abstractcontroller.h
+++ base/abstractcontroller.h
@@ -3,6 +3,11 @@
 
 #include <QObject>
 
+
+/* TODO: fill in this abstract class with virtual methods that correspond to
+ * generic controls for a game controller.
+ * https://github.com/OregonStateMarsRover/2011
+ */
 class AbstractController : public QObject
 {
     Q_OBJECT

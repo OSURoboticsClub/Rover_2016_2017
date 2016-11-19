7b8d754fa7c5de8848eb91a234905fb8c7137e67
Nick Ames <nick@fetchmodus.org>
2016-11-18 17:13:35 -0800

    Tracking down problems in miniboard uart; it's not a bad isr.

--- miniboard/firmware/src/miniboard.c
+++ miniboard/firmware/src/miniboard.c
@@ -24,6 +24,21 @@ void debugging_info_trigger(){
 	
 }
 
+/* ISR that fires if an interrupt is enabled without a corresponding handler. */
+ISR(BADISR_vect){
+	DDRB |= _BV(PB7);
+	while(1){
+		PORTB |= _BV(PB7);
+		_delay_ms(100);
+		PORTB &= ~_BV(PB7);
+		_delay_ms(200);
+		PORTB |= _BV(PB7);
+		_delay_ms(300);
+		PORTB &= ~_BV(PB7);
+		_delay_ms(300);
+	}
+}
+
 /* Setup all peripherals and subsystems. */
 void init(void){
 	comm_init();
@@ -32,7 +47,7 @@ void init(void){
 }
 
 int main(void){
-	//init();
+	init();
 	
 	while(1){
 		uint8_t *str = (uint8_t *) "Hello 123 456 789          Testing A B C";
@@ -42,7 +57,7 @@ int main(void){
 // 		uart_tx(COMM_UART, str, strlen(str));
 // 		uart_tx(COMM_UART, str, strlen(str));
 // 		uart_tx(COMM_UART, str, strlen(str));
-		_delay_ms(500);
+		_delay_ms(400);
 // 		uart_tx(COMM_UART, str, strlen(str));
 		//UDR0 = 'H';
  		DDRB |= _BV(PB7);
--- miniboard/firmware/src/uart.c
+++ miniboard/firmware/src/uart.c
@@ -114,20 +114,20 @@ void uart_disable(uint8_t uart){
 
 /* UART receive interrupts and handler. */
 static void uart_rx_isr(uint8_t uart){
-	if(uart == 0 && UART0RXHandler != NULL){
-		UART0RXHandler(UDRn(uart));
-	} else
-	if(uart == 1 && UART1RXHandler != NULL){
-		UART1RXHandler(UDRn(uart));
-	} else
-	if(uart == 2 && UART2RXHandler != NULL){
-		UART2RXHandler(UDRn(uart));
-	} else
-	if(uart == 3 && UART3RXHandler != NULL){
-		UART3RXHandler(UDRn(uart));
-	} else {
-		circ_add(UR + uart, UDRn(uart));
-	}
+// 	if(uart == 0 && UART0RXHandler != NULL){
+// 		UART0RXHandler(UDRn(uart));
+// 	} else
+// 	if(uart == 1 && UART1RXHandler != NULL){
+// 		UART1RXHandler(UDRn(uart));
+// 	} else
+// 	if(uart == 2 && UART2RXHandler != NULL){
+// 		UART2RXHandler(UDRn(uart));
+// 	} else
+// 	if(uart == 3 && UART3RXHandler != NULL){
+// 		UART3RXHandler(UDRn(uart));
+// 	} else {
+// 		circ_add(UR + uart, UDRn(uart));
+// 	}
 }
 
 ISR(USART0_RX_vect){
@@ -148,10 +148,10 @@ ISR(USART3_RX_vect){
 
 /* UART transmit (data register empty) interrupts and handler. */
 static void uart_tx_isr(uint8_t uart){
-	uint16_t r = circ_remove(UT + uart);
-	if(r != CIRC_EOF){
-		UDRn(uart) = r;
-	}
+// 	uint16_t r = circ_remove(UT + uart);
+// 	if(r != CIRC_EOF){
+// 		UDRn(uart) = r;
+// 	}
 }
 
 ISR(USART0_UDRE_vect){

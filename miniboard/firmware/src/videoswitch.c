caf7daaa2226125e4920a044e1e70150e1171511
rover <rover@build>
2017-01-21 15:55:29 -0800

    Inital video switch module.

--- miniboard/firmware/src/videoswitch.c
+++ miniboard/firmware/src/videoswitch.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <videoswitch.h>
+#include <avr/interrupt.h>
 
 /* Set to true by the callsign sender when a callsign has been entered. */
 extern bool CallsignSet;
@@ -18,3 +19,51 @@ extern bool CallsignSet;
  * ...
  * 6 = Input 6 */
 void videoswitch_select(uint8_t input);
+
+void videoswitch_select(uint8_t input){
+	
+	switch(input){
+		
+		case 1	:
+			PORTH &= ~_BV(PH6);
+			PORTB &= ~_BV(PB4);
+			PORTB &= ~_BV(PB5);
+			PORTB |= _BV(PB6);
+			
+		case 2	:
+			PORTH |= _BV(PH6);
+			PORTB &= ~_BV(PB4);
+			PORTB &= ~_BV(PB5);
+			PORTB |= _BV(PB6);
+			
+		case 3	:
+			PORTH &= ~_BV(PH6);
+			PORTB |= _BV(PB4);
+			PORTB &= ~_BV(PB5);
+			PORTB |= _BV(PB6);
+			
+		case 4	:
+			PORTH |= _BV(PH6);
+			PORTB |= _BV(PB4);
+			PORTB &= ~_BV(PB5);
+			PORTB |= _BV(PB6);
+			
+		case 5	:
+			PORTH &= ~_BV(PH6);
+			PORTB &= ~_BV(PB4);
+			PORTB |= _BV(PB5);
+			PORTB |= _BV(PB6);
+			
+		case 6	:
+			PORTH |= _BV(PH6);
+			PORTB &= ~_BV(PB4);
+			PORTB |= _BV(PB5);
+			PORTB |= _BV(PB6);
+			
+		default	:
+			PORTH |= _BV(PH6);
+			PORTB |= _BV(PB4);
+			PORTB |= _BV(PB5);
+			PORTB |= _BV(PB6);
+	}
+}

8c0249a4166074aa2d02c632c3ac00dc7b562025
Nick Ames <nick@fetchmodus.org>
2017-04-15 14:56:43 -0700

    Got no-comm timeout working.

--- miniboard/firmware/src/miniboard.c
+++ miniboard/firmware/src/miniboard.c
@@ -74,12 +74,20 @@ void reset_timeout_timer(void){
 	TCCR4B = 0;
 	TCNT4 = 0;
 	OCR4A = (uint16_t) 15625*COMM_TIMEOUT;
-	TIMSK5 = _BV(OCIE4A);
+	TIMSK4 = _BV(OCIE4A);
 	TCCR4B = _BV(CS42) | _BV(CS40); /* Set 1024 prescaler. */
 	CommTimedOut = false;
+	
+	//TODO
+	//Arduino pin 7 = PH4
+	DDRH |= _BV(PH4);
+	PORTH &= ~_BV(PH4);
 }
 
 ISR(TIMER4_COMPA_vect){
+	//TODO
+	PORTH |= _BV(PH4);
+	
 	CommTimedOut = true;
 	TCCR4B = 0;
 }

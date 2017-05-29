26e038f2bf7d828885396936814080e6a2b88dbb
Nick Ames <nick@fetchmodus.org>
2017-05-29 13:56:52 -0700

    First cut of direct control.

--- miniboard/firmware/src/miniboard.c
+++ miniboard/firmware/src/miniboard.c
@@ -151,18 +151,23 @@ uint8_t clamp127(int8_t value){
 #define MODE_SWITCH SWE
 #define TURN_SWITCH SG
 #define PAUSE_SWITCH SF
-#define DRIVE_LEFT JOY_LV
-#define DRIVE_RIGHT JOY_RV
-#define ARM_BASE JOY_LH
-#define ARM_BICEP JOY_LV
-#define ARM_FOREARM JOY_RV
-#define ARM_PITCH JOY_RH
-#define ARM_GRABBER SIDE_L
-#define ARM_EE SIDE_R
+#define DRIVE_LEFT Data->fr_joylv 
+#define DRIVE_RIGHT Data->fr_joyrv
+#define ARM_BASE Data->fr_joylh
+#define ARM_BICEP Data->fr_joylv
+#define ARM_FOREARM Data->fr_joyrv
+#define ARM_PITCH Data->fr_joyrh
+#define ARM_GRABBER Data->fr_sidel
+#define ARM_EE Data->fr_sider
 #define CAMERA_SELECT SH
-#define PAN POT_R
-#define TILT POT_L
-#define ARM_DRILL SIDE_L
+#define PAN Data->fr_potr
+#define TILT Data->fr_potl
+#define ARM_DRILL Data->fr_sidel
+
+/* Wrapper for code reuse */
+int8_t joy_ch(int8_t val){
+	return val;
+}
 	
 /* Return a joystick input switch value. */
 bool switch_ch(uint8_t index){
@@ -176,21 +181,24 @@ bool switch_ch(uint8_t index){
 /* Set motor speeds based on joystick inputs, if
  * enable bit is set. */
 void direct_control(void){
+	static uint8_t prev_pause;
+	static uint8_t prev_cam_select;
+	uint8_t cam_select;
 	bool quit;
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
 		quit = !(Data->xbox_buttons_high & _BV(7));
-		Data->buttons_high &= ~_BV(7);
+		Data->xbox_buttons_high &= ~_BV(7);
 	}
 	if(quit)return;
 	
-	if(switch_ch(PAUSE_SWITCH) == SW_FORWARD){
+	if(!switch_ch(PAUSE_SWITCH)){
 		Data->pause_state = 1;
 	} else {
 		Data->pause_state = 0;
 	}
-	Data->pan_speed = -joy_ch(PAN);
-	Data->tilt_speed = joy_ch(TILT);
-	cam_select = switch_ch(CAMERA_SELECT) == SW_BACK;
+	//Data->pan_speed = -joy_ch(PAN);
+	//Data->tilt_speed = joy_ch(TILT);
+	cam_select = switch_ch(CAMERA_SELECT);
 	if(cam_select && !prev_cam_select){
 		Data->selected_camera++;
 		if(Data->selected_camera > 6){
@@ -198,10 +206,10 @@ void direct_control(void){
 		}
 	}
 	prev_cam_select = cam_select;
-	if(switch_ch(MODE_SWITCH) == SW_FORWARD){
+	if(!switch_ch(MODE_SWITCH)){
 		/* Mode 1 - Drive */
-		int8_t left = joy_ch(JOY_LV);
-		int8_t right = joy_ch(JOY_RV);
+		int8_t left = joy_ch(DRIVE_LEFT);
+		int8_t right = joy_ch(DRIVE_RIGHT);
 		Data->l_f_drive = left;
 		Data->l_m_drive = left;
 		Data->l_b_drive = left;
@@ -217,16 +225,11 @@ void direct_control(void){
 		Data->arm_motor_3 = joy_ch(ARM_FOREARM);
 		Data->arm_motor_5 = 0;
 		Data->ee_speed = -8*joy_ch(ARM_EE);
-		if(switch_ch(MODE_SWITCH) == SW_MIDDLE){
-			/* Arm mode 1 - Grabber */
-			Data->arm_mode = 1;
-			Data->grabber_rotation_speed = 8*joy_ch(ARM_PITCH);
-			Data->grabber_speed = 8*joy_ch(ARM_GRABBER);
-		} else {
-			/* Arm mode 2 - Container Sealer */
-			Data->arm_mode = 2;
-			Data->arm_motor_4 = joy_ch(ARM_DRILL);
-		}
+		Data->arm_mode = 1;
+		Data->grabber_rotation_speed = 8*joy_ch(ARM_PITCH);
+		Data->grabber_speed = 8*joy_ch(ARM_GRABBER);
+// 		Data->arm_mode = 2;
+// 		Data->arm_motor_4 = joy_ch(ARM_DRILL);
 	}
 }
 	
